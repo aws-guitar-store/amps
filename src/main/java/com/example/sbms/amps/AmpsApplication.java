533689207907ce2e930f89d44d02cd12c18d9da7
Jeromy Van Dusen <jeromy.vandusen@gmail.com>
2020-10-28 10:34:36 -0500

    Create project and implement endpoint to get a list of amps

---
+++ src/main/java/com/example/sbms/amps/AmpsApplication.java
@@ -0,0 +1,11 @@
+package com.example.sbms.amps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AmpsApplication.class, args);
+	}
+}
